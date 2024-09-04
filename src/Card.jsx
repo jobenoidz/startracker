f6ef7c7f3ff6a0fdb3bac708fddf0cff64ca21aa
jobenoidz <jlaguirre@up.edu.ph>
2024-09-05 00:29:57 +0800

    added color gifs

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import './Card.css';
 
+import yellow from './yellow.gif';
+import green from './green.gif';
+import purple from './purple.gif';
+import blue from './blue.gif';
+import black from './black.gif';
+
 const Card = ({ data }) => {
-  const { Name, Start, Progress, End, Unit } = data;
+  const { Name, Start, Progress, End, Unit, Color } = data;
 
   function getProgress(exp) {
     var curr_exp = (exp % 12.5 / 12.5) * 100;
     return curr_exp.toFixed(0); // Multiply by 100 to get percentage
   }
 
+  function getColor(color) {
+    switch (color) {
+      case "yellow":
+        return yellow;
+      case "green":
+        return green;
+      case "purple":
+        return purple;
+      case "blue":
+        return blue;
+      case "black":
+        return black;
+      default:
+        break;
+    }
+  }
+
   const progressPercent = getProgress(Progress);
+  const gifColor = getColor(Color)
 
   return (
     <tr className="card">
@@ -23,7 +47,7 @@ const Card = ({ data }) => {
         <div className='bar'>
           <div
             className='bar-bg'
-            style={{ clipPath: `inset(0 ${100 - progressPercent}% 0 0)` }}
+            style={{ clipPath: `inset(0 ${100 - progressPercent}% 0 0)`, backgroundImage: `url(${gifColor})` }}
           ></div>
           {progressPercent >= 30 ? (
             <div
